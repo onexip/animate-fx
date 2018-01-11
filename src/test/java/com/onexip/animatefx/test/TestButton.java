ed5f743b777a0653f84437d7be3587c9b577243f
Tobi <bley@onexip.com>
2018-01-11 23:07:12 +0100

    first initial commit

---
+++ src/test/java/com/onexip/animatefx/test/TestButton.java
@@ -0,0 +1,26 @@
+package com.onexip.animatefx.test;
+
+import com.onexip.animatefx.transitions.CachedTimelineTransition;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+
+public class TestButton extends Button {
+    private CachedTimelineTransition transition;
+
+    public TestButton(Class<? extends CachedTimelineTransition> transition) {
+        super(transition.getSimpleName().replace("Transition",""));
+
+
+        try {
+            this.transition = transition.getConstructor(Node.class).newInstance(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        this.setOnMouseClicked((e) -> startTransition());
+    }
+
+    private void startTransition() {
+        this.transition.play();
+    }
+}
